21be2f8c7e4ca9057124088d794916f68afde87b
AndreaGhizzoni <andrea.ghz@gmail.com>
2013-11-09 15:55:44 +0100

    unzip function has been moved in ioutil lib

--- src/com/hackcaffebabe/mtg/gui/frame/listener/ImportActionListener.java
+++ src/com/hackcaffebabe/mtg/gui/frame/listener/ImportActionListener.java
@@ -9,13 +9,8 @@ import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
 import javax.swing.filechooser.FileFilter;
@@ -51,51 +46,19 @@ public class ImportActionListener implements ActionListener
 		if(f.showDialog( null, "Open" ) == JFileChooser.APPROVE_OPTION) {
 			JMenuItem src = (JMenuItem) e.getSource();
 			src.setCursor( new Cursor( Cursor.WAIT_CURSOR ) );
-			List<File> lst = unzip( f.getSelectedFile(), new File( DBCostants.JSON_PATH ) );
-			StoreManager.getInstance().refresh();
-			refreshMTGTable();
-			displayUnzippedFiles( lst );
+			List<File> lst;
+			try {
+				Logger.getInstance().write( Tag.DEBUG, f.getSelectedFile() );
+				lst = PathUtil.unZip( f.getSelectedFile(), new File( DBCostants.JSON_PATH ) );
+				StoreManager.getInstance().refresh();
+				refreshMTGTable();
+				displayUnzippedFiles( lst );
+			} catch( IOException e1) {
+				Logger.getInstance().write( Tag.ERRORS, e1.getMessage() );
+				e1.printStackTrace( Logger.getInstance().getPrintStream() );
+			}
 			src.setCursor( null );
 		}
 
-	}
-
-	private List<File> unzip(File srcZip, File dstFolder){
-		Logger.getInstance().write( Tag.DEBUG, "upzip called." );
-
-		List<File> lstFileLoded = new ArrayList<>();
-		try {
-			ZipInputStream zis = new ZipInputStream( new FileInputStream( srcZip ) );
-			Logger.getInstance().write( Tag.DEBUG, "zip opened correctly" );
-
-			ZipEntry ze = zis.getNextEntry();
-			while( ze != null ) {
-				File newFile = new File( dstFolder + PathUtil.FILE_SEPARATOR + ze.getName() );
-
-				//create the file. if file already exists, skip it
-				if(new File( newFile.getParent() ).mkdirs()) {
-					FileOutputStream fos = new FileOutputStream( newFile );
-					int len;
-					byte[] buffer = new byte[1024];
-					while( (len = zis.read( buffer )) > 0 ) {
-						fos.write( buffer, 0, len );
-					}
-					fos.close();
-					lstFileLoded.add( newFile );
-				}
-
-				ze = zis.getNextEntry();
-			}
-
-			zis.closeEntry();
-			zis.close();
-
-		} catch(IOException ex) {
-			ex.printStackTrace( Logger.getInstance().getPrintStream() );
-			return null;
-		}
-
-		Logger.getInstance().write( Tag.DEBUG, "done" );
-		return lstFileLoded;
 	}
 }
