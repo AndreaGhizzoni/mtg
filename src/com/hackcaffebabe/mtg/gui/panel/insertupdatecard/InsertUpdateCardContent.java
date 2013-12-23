b875f21344459f6243c9ceee3aaec03415571c10
AndreaGhizzoni <andrea.ghz@gmail.com>
2013-12-23 10:09:55 +0100

    added keyboard shortcut for update and save card

--- src/com/hackcaffebabe/mtg/gui/panel/insertupdatecard/InsertUpdateCardContent.java
+++ src/com/hackcaffebabe/mtg/gui/panel/insertupdatecard/InsertUpdateCardContent.java
@@ -25,6 +25,7 @@ import it.hackcaffebabe.logger.Logger;
 import it.hackcaffebabe.logger.Tag;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
@@ -133,6 +134,7 @@ public class InsertUpdateCardContent extends JPanel
 					disableUnnecessaryComponentsForUpdates();
 				}
 				btnSaveOrUpdate.setText( cardToUpdate == null ? "Save" : "Update" );
+				btnSaveOrUpdate.setMnemonic( cardToUpdate == null ? KeyEvent.VK_S : KeyEvent.VK_U );
 			}
 		} );
 	}
